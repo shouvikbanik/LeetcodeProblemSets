16e58af82521a5dfdfabce57e861c11281d3ab15
BanikShouvik <shouvik.banik@intel.com>
2022-10-29 14:30:24 +0530

    [Shouvik] 1.TwoSum

---
+++ two_sum.py
@@ -0,0 +1,6 @@
+#naive solution
+def twoSum(nums,target):
+    for i in range(len(nums)-1):
+        for j in range(i+1,len(nums)):
+            if nums[i]+nums[j]==target:
+                return [i,j]
